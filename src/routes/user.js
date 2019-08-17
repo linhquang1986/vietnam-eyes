72558923b88b632c288d2aa8bf119a4cc81f9806
QuangNguyen <quang.nguyend@s3corp.com.vn>
2019-08-17 23:17:50 +0700

    excute python script

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,30 @@
 import { Router } from 'express';
 
 const router = Router();
+const path = require('path')
+const { spawn } = require('child_process')
 
+/**
+ * Run python script, pass in `-u` to not buffer console output 
+ * @return {ChildProcess}
+ */
+function runScript() {
+  return spawn('python', [
+    "-u",
+    path.join(__dirname, '../core/test.py'),
+    "--foo", "some value for foo",
+  ]);
+}
+const subprocess = runScript()
 router.get('/', (req, res) => {
   return res.send(Object.values(req.context.models.users));
+});
+
+router.get('/training', (req, res) => {
+  
+  res.set('Content-Type', 'text/plain');
+  subprocess.stdout.pipe(res);
+  //return res.send(req.context.models.users[req.params.userId]);
 });
 
 router.get('/:userId', (req, res) => {
